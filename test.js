696d750b93909bb2dc82ea68c4dbd1bf82189602
steven <slstevenlam@gmail.com>
2016-01-08 00:47:55 -0800

    init repo

---
+++ test.js
@@ -0,0 +1,19 @@
+// for (var obj in opt1) {
+//     console.log(opt1[obj])
+//    for (var option in opt1.options){
+//     //console.log(opt1.options[option].price);
+//     if(opt1.options[option].price !== 0){
+//     console.log(option, opt1.options[option]);
+//     }
+//   }
+//  }
+
+
+// function merge_options(obj1,obj2){
+//     var obj3 = {};
+//     for (var attrname in obj2) { obj3[attrname] = obj2[attrname]; }
+//     for (var attrname in obj1) { obj3[attrname] = obj1[attrname]; }
+//     return obj3;
+// }
+
+// console.log(merge_options(opt1, opt2));
